8994d81c5eb817873f400c90bf499a52e0f453db
otsuneko <tkobayashi1021@gmail.com>
2022-08-04 21:55:15 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Leaflet from 'leaflet'
+import React, { Component } from 'react';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import LiveCamMap from './components/livecam-map'
+
+Leaflet.Icon.Default.imagePath =
+  '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <LiveCamMap />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/livecam-map.js
@@ -0,0 +1,44 @@
+import React, { Component, useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { LatLng } from 'leaflet';
+import './livecam-map.css';
+import { useHistory, useLocation } from 'react-router-dom';
+
+function LocationMarker() {
+    const [position, setPosition] = useState(new LatLng(35.6809591, 139.7673068))
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        if(position!=null) {
+          setPosition(new LatLng( latitude, longitude ));
+        }
+      });
+  }
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    )
+  }
+
+export default class LiveCamMap extends Component {
+  render() {
+    const position = [35.6809591, 139.7673068];
+    const zoom = 13
+    const scrollWheelZoom = true
+    return (
+      <MapContainer center={position} zoom={zoom} scrollWheelZoom={scrollWheelZoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationMarker />
+        <Marker position={position}>
+          <Popup>
+            Hello World! <br />
+          </Popup>
+        </Marker>
+      </MapContainer>
+    )
+  }
+}
